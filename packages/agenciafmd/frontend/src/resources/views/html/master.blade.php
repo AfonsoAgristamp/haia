3816dedf0977c942e324054b2d6711f7c0ed5b0e
Afonso <afonsobezerra199@gmail.com>
2022-09-09 14:27:09 -0300

    Google Tag analytcs

--- packages/agenciafmd/frontend/src/resources/views/html/master.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/html/master.blade.php
@@ -48,13 +48,7 @@
     
     <meta name="facebook-domain-verification" content="la8ajnq3cykheuyr7wh1k1m7wohjrv" />
 	
-	<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TBTVNVF');</script>
-<!-- End Google Tag Manager -->
+
 </head>
 <body class="{{ ($bodyClass) ?? '' }}">
 
