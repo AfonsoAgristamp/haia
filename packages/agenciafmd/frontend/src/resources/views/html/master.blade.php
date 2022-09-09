631255fddb1c95de2a6d527e551b7f6d75cf4418
Afonso <afonsobezerra199@gmail.com>
2022-09-09 13:41:43 -0300

    fix name title

--- packages/agenciafmd/frontend/src/resources/views/html/master.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/html/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5.0">
 
-    <title>@yield('title', '') | {{ config('app.name') }}</title>
+    <title>@yield('title', '') | {{ Haia Boa Vista }}</title>
     <meta name="description" content="@yield('description', '')">
     <meta name="author" content="@yield('author', 'F&MD')">
 
