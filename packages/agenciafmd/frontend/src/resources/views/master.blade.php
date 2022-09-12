f16f4f4e1c24380af1d30b81d81008728298e73b
Afonso <afonsobezerra199@gmail.com>
2022-09-12 17:51:32 -0300

    tag

--- packages/agenciafmd/frontend/src/resources/views/master.blade.php
+++ packages/agenciafmd/frontend/src/resources/views/master.blade.php
@@ -121,6 +121,25 @@
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBTVNVF"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '276621601327127');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=276621601327127&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
 <x-admix-analytics::gtm.body/>
 
 @stack('header')
@@ -140,24 +159,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 @include('agenciafmd/frontend::partials.message')
 
 @stack('scripts')
-
-<!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '276621601327127');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=276621601327127&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
 
 @if (config('app.env') === 'local')
     <script id="__bs_script__">//<![CDATA[
